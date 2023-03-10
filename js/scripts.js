124778cb2653ee267d10c7358ed00c79c5f718c9
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-03-09 21:38:41 -0500

    Added new array variable and added 4 objects to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-alert('Hello World');
-let favoriteFood = 'Chicken Tikka Tandoori';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+    {
+        name: "Noctowl",
+        height: 5,
+        type: ['Normal','Flying'],
+        abilities: ['Keen Eye','Insomnia']
+
+    },
+    {
+        name: "Charizard",
+        height: 5,
+        type: ['Fire','Flying'],
+        abilities: ['Blaze']
+    },
+    {
+        name: "Arcanine",
+        height: 6,
+        type: ['Fire'],
+        abilities: ['Intimidate','Flash Fire']
+    },
+    {
+        name: "Moltres",
+        height: 6,
+        type: ['Fire', 'Flying'],
+        abilities: ['Pressure']
+    }
+]
