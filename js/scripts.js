3e5298e97e33255dc450be32d757b63df538dbad
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-03-10 18:19:15 -0500

    Added For loop to iterate over the pokemonList array and print the name and height of each

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,18 @@ pokemonList = [
         abilities: ['Pressure']
     }
 ]
+
+/* Opening/creating a div element, and opening/creating an unordered list */
+document.write('<div><ul>');
+
+/* Iterating over the array and displaying the name and height of each */
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 5) {
+        document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!</li>`);
+    } else {
+        document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height})</li>`);
+    }
+}
+
+/* Closing the unordered list and div elements */
+document.write('</ul></div>');
