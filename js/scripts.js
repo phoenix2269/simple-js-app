a6449424355b138ede0cac27f6914118a91d550b
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-03-11 19:41:31 -0500

    Updated code to use forEach loop and wrap list in an IIFE
    Updated code to use forEach loop and wrap list in an IIFE.  Also added ADD and getAll functions to IIFE.  Also added ADDV function to check user input before adding to object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,59 @@
-let pokemonList = [];
-pokemonList = [
-    {
-        name: "Noctowl",
-        height: 5,
-        type: ['Normal','Flying'],
-        abilities: ['Keen Eye','Insomnia']
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Noctowl",
+            height: 5,
+            type: ['Normal','Flying'],
+            abilities: ['Keen Eye','Insomnia']
 
-    },
-    {
-        name: "Charizard",
-        height: 5,
-        type: ['Fire','Flying'],
-        abilities: ['Blaze']
-    },
-    {
-        name: "Arcanine",
-        height: 6,
-        type: ['Fire'],
-        abilities: ['Intimidate','Flash Fire']
-    },
-    {
-        name: "Moltres",
-        height: 6,
-        type: ['Fire', 'Flying'],
-        abilities: ['Pressure']
+        },
+        {
+            name: "Charizard",
+            height: 5,
+            type: ['Fire','Flying'],
+            abilities: ['Blaze']
+        },
+        {
+            name: "Arcanine",
+            height: 6,
+            type: ['Fire'],
+            abilities: ['Intimidate','Flash Fire']
+        },
+        {
+            name: "Moltres",
+            height: 6,
+            type: ['Fire', 'Flying'],
+            abilities: ['Pressure']
+        }
+    ];
+
+    function addv(userInput) {
+        return (typeof userInput === "object" && Object.keys(userInput) === Object.keys(pokemonList));
     }
-]
+
+    function add(pokemon) {
+        if (addv(pokemon)) {
+            pokemonList.push(pokemon);
+        } else {
+            document.write("Input is not a valid object!");
+        }
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add,
+        getAll
+    };
+})();
 
 /* Opening/creating a div element, and opening/creating an unordered list */
 document.write('<div><ul>');
 
 /* Iterating over the array and displaying the name and height of each */
-for (let i = 0; i < pokemonList.length; i++) {
+/* for (let i = 0; i < pokemonList.length; i++) {
     let name = pokemonList[i].name;
     let height = pokemonList[i].height;
     if (height > 5) {
@@ -39,7 +61,16 @@ for (let i = 0; i < pokemonList.length; i++) {
     } else {
         document.write(`<li>${name} (height: ${height})</li>`);
     }
-}
+} */
+pokemonRepository.getAll().forEach(function(pokemon) {
+    let name = pokemon.name;
+    let height = pokemon.height;
+    if (height > 5) {
+        document.write(`<li>${name} (height: ${height}) - Wow, that's big!</li>`);
+    } else {
+        document.write(`<li>${name} (height: ${height})</li>`);
+    }
+});
 
 /* Closing the unordered list and div elements */
 document.write('</ul></div>');
