674ae8815ba62ad8a064c8dad6b8856ee70d5832
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-03-13 12:41:24 -0400

    Rename selectedLi to liButton

--- js/scripts.js
+++ js/scripts.js
@@ -78,7 +78,7 @@ pokemonRepository.getAll().forEach(function(pokemon) {
     let listItem = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
-    button.classList.add('selectedLi');
+    button.classList.add('liButton');
     listItem.appendChild(button);
     ulList.appendChild(listItem);
 });
